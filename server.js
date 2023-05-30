6c5584af16b5eb8b6bde161c295debf10109894e
akanksha2227 <kumariakanksha338@gmail.com>
2023-05-30 14:23:37 +0530

    commit

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.static("routes"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static(path.join(__dirname,'./opt/render/project/src/build')));
+app.use(express.static(path.join(__dirname,'./client/build')));
 
 app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./opt/render/project/src/build/index.html'));
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
 })
 
 const connection = require("./services/dbConnection");
